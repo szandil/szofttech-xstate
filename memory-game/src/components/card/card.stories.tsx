05ad311e371b7b08820fe5ed324cb688462b082f
ladanyisz <ladanyisz@lauder.hu>
2022-03-05 22:57:17 +0100

    storybook init

---
+++ memory-game/src/components/card/card.stories.tsx
@@ -0,0 +1,8 @@
+import { Meta } from "@storybook/react";
+import { Card } from "./card";
+
+export default {
+    title: 'Card'
+} as Meta;
+
+export const BaseCard = () => <Card />
---
+++ memory-game/src/components/card/card.tsx
@@ -0,0 +1,9 @@
+import styles from './card.module.css';
+
+export const Card = () => {
+    return (
+        <span className={styles.card}>
+            THIS IS A CARD
+        </span>
+    );
+};
