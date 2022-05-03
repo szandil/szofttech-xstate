4bc4de7fd130ca6a333f8e1101272a2d109bcaee
ladanyisz <92325224+szandil@users.noreply.github.com>
2022-05-03 19:29:01 +0200

    Update game-machine.js

--- memory-game-separate-context-xstate/state-machine/game-machine.js
+++ memory-game-separate-context-xstate/state-machine/game-machine.js
@@ -1,13 +1,14 @@
 const { createMachine } = require("xstate");
 
 const gameMachine = 
-/** @xstate-layout N4IgpgJg5mDOIC5RQIYFswDoDuKCWALngHZQAEAZgPYBOZqGAxAMoAqAggEqsD6AcgFEA6jwDi7ALIDEoAA5VYhPFWIyQAD0QBaAIwBmACyYAHACYA7AAZjO8+b0Xj1gDQgAnttMBWHZi9eATgA2IIMfYwC9IONzAF9Y1wYsYioyAGMUGghYSgAbPFlZSEYAMQAZAEkABTV5RSIVNU0Eb18InRC9Jy7jAwNjVw8ELVa-QKCvQyCdXr09eMT0LBUwdMyIPIKiiFLKmqQQOqVGg+bJ4xNzIMsbq-N+0wH3Tx8x4MmDadnDBZAkzAI2FSGSyOQo+UKxVqCmOqlOiDClj8Bki-gCVi8Bks5kG2n0ekweiu4X62Ms-iC8QSIBSEDgan+uCUpEotHoS2h9WUcNAzS0cyCJi8xj0lneATFOlxw285jeEwMUwMOmsv3+KTWoM2kIgnNhTUQr3aITMpiC5lMOgCpmlIy8crRCqVKuMaqWmBWmo24K2kD1DR5GkQczlpj0k3RxhsIXROOeMvt8qun3Jkbi1P+gOB6zBEO2-u5BoQ0wJ5oCgU+pksyoMtrNctT0VMKNsegCroz7rSVFyuTAaSILNk+BoBZOvMQpjDJksQXDgX8KrFcaGujblwmc0V5ki5i8pjdGEwSTIVAAbmBRwcjgGiyMVZhLJa92ErDphTNbTorZgrMZJpK4YGBYXiHmAY6BnyURIv+IpihMEoBFK8b8lij7jPckQWNYlJUkAA */
+/** @xstate-layout N4IgpgJg5mDOIC5RQIYFswDoDuKCWALngHZQAEAZgPYBOZqGAxAMoAqAggEqsD6AcgFEA6jwDi7ALIDEoAA5VYhPFWIyQAD0QBaAIwBmACyYAHACYA7AAZjO8+b0Xj1gDQgAnttMBWHZi9eATgA2IIMfYwC9IONzAF9Y1wYsYioyAGMUGghYSgAbPFlZSEYAMQAZAEkABTV5RSIVNU0Eb18InRC9Jy7jAwNjVw8ELVa-QKCvQyCdXr09eMT0LBUwdMyIPIKiiFLKmqQQOqVGg+bJ4xNzIMsbq-N+0wH3Tx8x4MmDadnDBZAkzAI2FSGSyOQo+UKxVqCmOqlOiDClj8Bki-gCVi8Bks5kG2n0ekweiu4X62Ms-iCv3+aSouVyYDSRFIZFk+BojGh9WUcNAzV0liMphu+ixOkCAT6XlxCD0AUw5lMAWMNll5h0AXVlISfyWmCSZCoADcwOy2FxeIIROIpJzYU08Tc-LLZV4gkLsaYLNLdIZ5ZYHJ0ggFgiFjPFtSkIHA1P9cEpmdQ6ElbQ0eRptHMgiYvMY9JZ3gF8zpvd5zG8JgYpgYdNYqbqUmtQZtIRAU9z7QhXu1Q56ggr1aYS14y2iK1Wa2Htf8Vo2NuCtpA2ydeYg5mXTHpJujlR0g3YhyPxldPuTt3Ep7rAcD1mCIdsl2nmtMCX2AoFPkLqwYS33MKfoqYKK2LKk6LBgmA0nSDJMuQrJ4DQD4duqFxWF+hbYg8b7evocoxBMpj6OYrqTNEdbgfqRomoh8LDKYv7BhYQaWG6GozNh-omEq3gGBuRI3L0ZFgNRK7DFESLGDmeYFkW3qGEi-5vv6ExiuGsRAA */
 createMachine({
   initial: "waiting for game",
   states: {
     "waiting for game": {
       on: {
         START_NEW_GAME: {
+          actions: "initGame",
           target: "no cards flipped",
         },
       },
@@ -29,16 +30,37 @@ createMachine({
     "two cards flipped": {
       always: [
         {
+          actions: "collectPair",
           cond: "pairFound",
           target: "collecting pair",
         },
         {
+          actions: ["resetFlippedCards", "nextPlayer"],
           target: "no cards flipped",
         },
       ],
     },
-    "collecting pair": {},
-    "game over": {},
+    "collecting pair": {
+      always: [
+        {
+          description: "\n",
+          cond: "allCardsCollected",
+          target: "game over",
+        },
+        {
+          actions: "resetFlippedCards",
+          target: "no cards flipped",
+        },
+      ],
+    },
+    "game over": {
+      on: {
+        START_NEW_GAME: {
+          actions: "initGame",
+          target: "no cards flipped",
+        },
+      },
+    },
   },
   id: "game",
 });
